8056b28d3c4c78e581f275d41bc8dfdeff9ce342
Hinal0101 <hinaljethava112002@gmail.com>
2023-10-25 18:25:46 +0530

    permutations using backtracking

---
+++ permutations.java
@@ -0,0 +1,20 @@
+package BackTracking_in_Java;
+
+public class permutations {
+    public static void print_perm(String str,String perm,int idx){
+    if(str.length()==0){
+        System.out.println(perm);
+        return;
+    }
+    for(int i=0;i<str.length();i++){
+        char currchar=str.charAt(i);
+        String newstr=str.substring(0, i)+str.substring(i+1);
+        print_perm(newstr, perm+currchar, idx+1);
+    }
+
+}
+    public static void main(String args[]){
+        String str="ABC";
+        print_perm(str, "", 0);
+    }
+}
